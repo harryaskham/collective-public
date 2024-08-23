55867352422037b5befefce6e77105cd5830539f
Harry Askham <harryaskham@gmail.com>
2024-08-23 10:40:35 +0100

    [rapid commit; squash later]

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,12 +5,8 @@
 
   modifications = final: prev: {
     handheld-daemon = final.new.handheld-daemon;
-    # TODO: Messy below, probably only need the top one
     python3 = prev.python3.override {
       packageOverrides = self: super: final.new.pythonPackages;
     };
-    pythonPackages = prev.pythonPackages // final.new.pythonPackages;
-    python3Packages = prev.python3Packages // final.new.pythonPackages;
-    python312Packages = prev.python312Packages // final.new.pythonPackages;
   };
 }
