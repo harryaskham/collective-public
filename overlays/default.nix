49116cb1690aea7bb0ba9e6b4f58c8d8d4c7ea16
Harry Askham <harryaskham@gmail.com>
2024-10-08 17:45:28 +0100

    [rapid commit; squash later]

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,14 +1,24 @@
 {inputs, ...}:
 
-{
-  additions = final: _prev: import ../pkgs final.pkgs;
-
-  modifications = final: prev: rec {
+let
+  image-go-nord-overlay = self: super: {
+    image-go-nord = super.image-go-nord.overrideAttrs (_: {
+      doCheck = false;
+      propagatedBuildInputs =
+        let missingBuildInputs = with super; [numpy ffmpeg-python requests];
+        in super.image-go-nord.propagatedBuildInputs ++ missingBuildInputs;
+    });
+  };
+in final: prev:
+  (import ../pkgs final.pkgs)
+  // rec {
     python3 = prev.python3.override {
-      packageOverrides = self: super: super // final.toOverlay.pythonPackages;
+      packageOverrides = self: super:
+        super
+        // final.toOverlay.pythonPackages
+        // (image-go-nord-overlay self super);
     };
     python3Packages = final.python3.pkgs;
 
     handheld-daemon = final.toOverlay.handheld-daemon;
-  };
-}
+  }
