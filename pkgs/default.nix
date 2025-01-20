219f479877acaaa642479f79c28cc9b2a92a27f8
Harry Askham <harryaskham@gmail.com>
2025-01-20 10:43:12 +0000

    [rapid commit; squash later]

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,14 +1,14 @@
 { pkgs, ...}:
 
 let
-  handheld-daemon-ui-pkg = pkgs.callPackage ./handheld-daemon-ui.nix { };
-  pythonPackages = import ./pythonPackages { pkgs = pkgs; handheld-daemon-ui = handheld-daemon-ui-pkg; };
+  # handheld-daemon-ui-pkg = pkgs.callPackage ./handheld-daemon-ui.nix { };
+  pythonPackages = import ./pythonPackages { inherit pkgs; };
 in {
-  handheld-daemon =
-    pkgs.python3Packages.toPythonApplication
-      pythonPackages.handheld-daemon-hhd;
+  # handheld-daemon =
+  #   pkgs.python3Packages.toPythonApplication
+  #     pythonPackages.handheld-daemon-hhd;
   handheld-daemon-adjustor =
       pkgs.python3Packages.toPythonApplication
         pythonPackages.handheld-daemon-adjustor;
-  handheld-daemon-ui = handheld-daemon-ui-pkg;
+  # handheld-daemon-ui = handheld-daemon-ui-pkg;
 }
--- pkgs/pythonPackages/default.nix
+++ pkgs/pythonPackages/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, handheld-daemon-ui, ... }:
+{ pkgs, ... }:
 
 rec {
   handheld-daemon-adjustor =
     pkgs.callPackage ./handheld-daemon/adjustor.nix {  };
-  handheld-daemon-hhd =
-    pkgs.callPackage ./handheld-daemon/hhd.nix {
-      adjustor = handheld-daemon-adjustor;
-      handheld-daemon-ui = handheld-daemon-ui;
-    };
+  # handheld-daemon-hhd =
+  #   pkgs.callPackage ./handheld-daemon/hhd.nix {
+  #     adjustor = handheld-daemon-adjustor;
+  #     handheld-daemon-ui = handheld-daemon-ui;
+  #   };
 }
