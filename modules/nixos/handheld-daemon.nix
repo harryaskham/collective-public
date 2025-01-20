6bb6f06d6bbd9105093cbefa133f78303a25d555
Harry Askham <harryaskham@gmail.com>
2025-01-20 10:52:29 +0000

    Adjustor option

--- modules/nixos/handheld-daemon.nix
+++ modules/nixos/handheld-daemon.nix
@@ -5,55 +5,19 @@ with lib;
 let
   cfg = config.handheld-daemon;
 in {
-  options = {
-    handheld-daemon = {
-      enable = mkEnableOption "Enable the handheld-daemon service.";
-      user = mkOption {
-        type = types.str;
-        description = "The user to run the daemon with.";
-      };
-      ui = {
-        enable = mkOption {
-          type = types.bool;
-          default = true;
-          description = "Enable the HHD UI overlay.";
-        };
-      };
-      adjustor = {
-        enable = mkOption {
-          type = types.bool;
-          default = true;
-          description = "Install the HHD Adjustor for TDP control. Note that this is bundled with HHD as a dependency so is always enabled with HHD; this just exposes the CLI tool and python library to the system.";
-        };
-      };
+  options.handheld-daemon.adjustor = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Install the HHD Adjustor for TDP control.";
     };
   };
 
-  config = mkIf cfg.enable (mkMerge [
+  config = mkIf cfg.adjustor.enable {
+    environment.systemPackages = with pkgs; [
+      handheld-daemon-adjustor
+      python3Packages.handheld-daemon-adjustor
+    ];
+  };
 
-    {
-      services.handheld-daemon = {
-        enable = true;
-        user = cfg.user;
-      };
-      environment.systemPackages = with pkgs; [
-        handheld-daemon
-        python3Packages.handheld-daemon-hhd
-      ];
-    }
-
-    (mkIf cfg.ui.enable ({
-      environment.systemPackages = with pkgs; [
-        handheld-daemon-ui
-      ];
-    }))
-
-    (mkIf cfg.adjustor.enable ({
-      environment.systemPackages = with pkgs; [
-        handheld-daemon-adjustor
-        python3Packages.handheld-daemon-adjustor
-      ];
-    }))
-
-  ]);
 }
