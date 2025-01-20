27f7ac518f90cfb8a77af7d0c575b0de6a3364f9
Harry Askham <harryaskham@gmail.com>
2025-01-20 11:57:18 +0000

    [rapid commit; squash later]

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -7,6 +7,6 @@ in {
     (pkgs.python3Packages.toPythonApplication
       pythonPackages.handheld-daemon-adjustor).overrideAttrs (oldAttrs: {
         # Include its own Python library in its runtime dependencies as it forks Python processes that import `hhd.adjustor`
-        propagatedBuildInputs = oldAttrs.propagatedBuildInputs ++ [ pythonPackages.handheld-daemon-adjustor ];
+        dependencies = oldAttrs.dependencies ++ [ pythonPackages.handheld-daemon-adjustor ];
       });
 }
