62a2701a775cc40473ca90f1d745fd82f4c30299
Harry Askham <harryaskham@gmail.com>
2025-01-20 12:58:39 +0000

    [rapid commit; squash later]

--- modules/nixos/handheld-daemon.nix
+++ modules/nixos/handheld-daemon.nix
@@ -31,7 +31,7 @@ in {
       services.handheld-daemon.package = pkgs.handheld-daemon.overrideAttrs (attrs: {
         dependencies = (attrs.dependencies or []) ++ (with pkgs; [
           handheld-daemon-adjustor
-          # python3Packages.handheld-daemon-adjustor
+          python3Packages.handheld-daemon-adjustor
         ]);
       });
     })
