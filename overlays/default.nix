7a117baf168260fa706ab04be5862bf30aecf5c2
Harry Askham <harryaskham@gmail.com>
2025-01-20 15:50:20 +0000

    [rapid commit; squash later]

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,7 @@
 {inputs, ...}:
 
 let
-  image-go-nord-overlay = self: super: {
+  imageGoNordOverlay = self: super: {
     image-go-nord = super.image-go-nord.overrideAttrs (_: {
       doCheck = false;
       propagatedBuildInputs =
@@ -9,23 +9,15 @@ let
         in super.image-go-nord.propagatedBuildInputs ++ missingBuildInputs;
     });
   };
-  handheld-daemon-overlay = final: prev: {
-    handheld-daemon = prev.handheld-daemon.overrideAttrs (attrs: {
-      dependencies = (attrs.dependencies or []) ++ [
-        final.pythonPackages.handheld-daemon-adjustor
-      ];
-    });
-  };
-in rec {
+in {
   python3Overlay = final: prev: rec {
     python3 = prev.python3.override {
       packageOverrides = (self: super:
         (import ../pkgs/pythonPackages { pkgs = prev; })
-        // (image-go-nord-overlay self super)
+        // (imageGoNordOverlay self super)
       );
     };
     python3Packages = prev.python3Packages // final.python3.pkgs;
   };
   packagesOverlay = final: prev: import ../pkgs { pkgs = prev; };
-  handheldDaemonOverlay = inputs.nixpkgs.lib.fixedPoints.composeManyExtensions [ python3Overlay handheld-daemon-overlay ];
 }
