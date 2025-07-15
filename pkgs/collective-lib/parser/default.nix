4661286065c307071e33dedc001cc0b6fe2a4582
Harry Askham <harryaskham@gmail.com>
2025-07-15 00:28:24 +0000

    [rapid commit; squash later]

--- pkgs/collective-lib/parser/default.nix
+++ pkgs/collective-lib/parser/default.nix
@@ -3,6 +3,18 @@
   ...
 }:
 
+# TODO
+# - complete parser:
+#   - floats paths nulls bools lists
+#   - lambdas
+#   - default arguments
+#   - .fieldaccess
+#   - let/in
+#   - inherits
+#   - assert, abort, other builtins
+# -
+# - retain position information
+
 let
   eval = collective-lib.eval;
   typed = collective-lib.typed;
@@ -11,37 +23,42 @@ let
 in 
   with typed;
 rec {
-  mk = {
-    assignment = lhsRhs: {
-      nodeType = "assignment";
-      identifier = lhsRhs.lhs;
-      value = lhsRhs.rhs;
-    };
 
-    attrs = assignments: {
-      nodeType = "attrs";
-      inherit assignments;
-    };
+  node = nodeType: args: {
+    nodeType = nodeType;
+  } // args;
 
-    identifier = name: {
-      nodeType = "identifier";
-      inherit name;
-    };
+  ast = {
+    assignment = lhsRhs:
+      node "assignment" {
+        identifier = lhsRhs.lhs;
+        value = lhsRhs.rhs;
+      };
 
-    indentString = s: {
-      nodeType = "indentString";
-      inherit s;
-    };
+    attrs = assignments:
+      node "attrs" {
+        inherit assignments;
+      };
 
-    normalString = s: {
-      nodeType = "normalString";
-      inherit s;
-    };
+    identifier = name:
+      node "identifier" {
+        inherit name;
+      };
 
-    int = i: {
-      nodeType = "int";
-      inherit i;
-    };
+    indentString = s:
+      node "indentString" {
+        inherit s;
+      };
+
+    normalString = s:
+      node "normalString" {
+        inherit s;
+      };
+
+    int = i:
+      node "int" {
+        inherit i;
+      };
   };
 
   p = with parsec; rec {
@@ -60,7 +77,7 @@ rec {
       (lhs: bind rhs 
       (rhs: pure { inherit lhs rhs; }));
 
-    identifier = fmap (matches: mk.identifier (head matches))(matching ''[a-zA-Z_][a-zA-Z0-9_\-\.]*'');
+    identifier = fmap (matches: ast.identifier (head matches))(matching ''[a-zA-Z_][a-zA-Z0-9_\-\.]*'');
 
     assignment =
       let lhs = choice [
@@ -68,17 +85,17 @@ rec {
         anyString
         interpolation
       ];
-      in spaced (fmap mk.assignment (infix2 lhs "=" expr));
+      in spaced (fmap ast.assignment (infix2 lhs "=" expr));
 
     assignments = many (thenSkip assignment semi);
 
-    attrs = spaced (fmap mk.attrs (between (sym "{") (sym "}") assignments));
+    attrs = spaced (fmap ast.attrs (between (sym "{") (sym "}") assignments));
 
-    int = fmap mk.int lexer.decimal;
-    signedInt = fmap mk.int (lexer.signed spaces lexer.decimal);
-    normalString = fmap mk.normalString lexer.stringLit;
-    # TODO: harder logic
-    indentString = fmap mk.indentString (between (string "''") (string "''") (fmap join (many (anyCharBut "'"))));
+    int = fmap ast.int lexer.decimal;
+    signedInt = fmap ast.int (lexer.signed spaces lexer.decimal);
+    normalString = fmap ast.normalString lexer.stringLit;
+    # TODO: indent strings require harder stopping logic
+    indentString = fmap ast.indentString (between (string "''") (string "''") (fmap join (many (anyCharBut "'"))));
     interpolation = between (string "\${") (string "}") expr;
     anyString = choice [
       normalString
@@ -97,9 +114,7 @@ rec {
     ]);
   };
 
-  parse = {
-    expr = s: parsec.runParser p.expr s;
-  };
+  parse = s: parsec.runParser p.expr s;
 
   read = rec {
 
@@ -138,25 +153,32 @@ rec {
         };
 
         identifier = {
-          normal = expectSuccess p.identifier "a" (mk.identifier "a");
-          startUnderscore = expectSuccess p.identifier "_a" (mk.identifier "_a");
-          numbersLater = expectSuccess p.identifier "_a0" (mk.identifier "_a0");
+          normal = expectSuccess p.identifier "a" (ast.identifier "a");
+          startUnderscore = expectSuccess p.identifier "_a" (ast.identifier "_a");
+          numbersLater = expectSuccess p.identifier "_a0" (ast.identifier "_a0");
           startNumber = expectError p.identifier "1a";
         };
 
         value = {
-          int = expectSuccess p.value "1" (mk.int 1);
-          normalString = expectSuccess p.value ''"a"'' (mk.normalString "a");
-          indentString = expectSuccess p.value "''a''" (mk.indentString "a");
+          int = expectSuccess p.value "1" (ast.int 1);
+          normalString = expectSuccess p.value ''"a"'' (ast.normalString "a");
+          indentString = expectSuccess p.value "''a''" (ast.indentString "a");
         };
 
         attrs = {
-          empty = expectSuccess p.attrs "{}" (mk.attrs []);
-          spacedEmpty = expectSuccess p.attrs "  \t \n {  \t\n }  \t \n" (mk.attrs []);
-          surroundSpacedEmpty = expectSuccess p.attrs "  \t \n {  \t\n }  \t \n" (mk.attrs []);
+          empty = expectSuccess p.attrs "{}" (ast.attrs []);
+          spacedEmpty = expectSuccess p.attrs "  \t \n {  \t\n }  \t \n" (ast.attrs []);
+          surroundSpacedEmpty = expectSuccess p.attrs "  \t \n {  \t\n }  \t \n" (ast.attrs []);
           singleAssignment = 
             expectSuccess p.attrs "{ a = 1; }"
-            (mk.attrs [ (mk.assignment { lhs = mk.identifier "a"; rhs = mk.int 1; }) ]);
+            (ast.attrs [ (ast.assignment { lhs = ast.identifier "a"; rhs = ast.int 1; }) ]);
+          singleAssignmentWithComment =
+            expectSuccess p.attrs ''
+            {
+              a = 1;  #a comment
+            }
+            ''
+            (ast.attrs [ (ast.assignment { lhs = ast.identifier "a"; rhs = ast.int 1; }) ]);
         };
       };
 
