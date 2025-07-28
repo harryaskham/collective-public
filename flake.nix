977b86d49df73db376b1deb262b013dbfc532b62
Harry Askham <harryaskham@gmail.com>
2025-07-29 00:57:03 +0100

    [rapid commit; squash later]

--- flake.nix
+++ flake.nix
@@ -22,13 +22,17 @@
         let 
           collective-lib = outputs.packages.${system}.collective-lib;
           pkgs = nixpkgs.legacyPackages.${system};
+          testedModules = import ./modules { inherit collective-lib; };
         in {
           lib = collective-lib;
           packages = import ./pkgs { inherit pkgs inputs; };
           devShells = { default = pkgs.mkShell {}; };
           # Modules still have _tests here; removed before applying them to
           # systems.
-          modules = import ./modules { inherit collective-lib; };
+          inherit testedModules;
+          modules = 
+            with collective-lib.tests;
+            removeTests (mapAttrs (_: removeTests) testedModules);
         }
       ) // {
         overlays = import ./overlays { inherit inputs; inherit (nixpkgs) lib; };
