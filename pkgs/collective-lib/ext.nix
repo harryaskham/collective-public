f1a3f416474396161be8c1c343e822ff505376b7
Harry Askham <harryaskham@gmail.com>
2025-08-12 12:06:05 +0100

    Pass inputs into reflect

--- pkgs/collective-lib/ext.nix
+++ pkgs/collective-lib/ext.nix
@@ -4,14 +4,15 @@
   # where NIX_PATH is set up with dependencies hard-coded into the WASM blob.
   wasm = rec {
     lib = import <nixpkgs/lib>;
+    collective-lib = import <collective/pkgs/collective-lib> args;
+    collective-lib-public = import <collective/collective-public/pkgs/collective-lib> args;
     args = {
       pkgs.lib = lib;
       pkgs.system = "x86_64-linux";
       inputs.nix-parsec = import <nix-parsec>;
-      inputs.collective-public.lib.${args.pkgs.system} =
-        import <collective/collective-public/pkgs/collective-lib> args;
+      inputs.collective-public.lib.${args.pkgs.system} = collective-lib-public;
+      inputs.nix-reflect.lib.${args.pkgs.system} = import <nix-reflect/lib> args;
     };
-    collective-lib = import <collective/pkgs/collective-lib> args;
     inherit (collective-lib) typed;
   };
 }
