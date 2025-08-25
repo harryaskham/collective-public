cc98dd63c3bc4964849a97f3520ad2d3f95e2005
Harry Askham <harryaskham@gmail.com>
2025-08-25 17:17:17 +0100

    [rapid commit; squash later]

--- modules/nixos/handheld-daemon.nix
+++ modules/nixos/handheld-daemon.nix
@@ -39,10 +39,16 @@ in {
         '';
 
         # Substitutions don't handle the udev rules at least in nixos-unstable 2025-08-25
-        postInstall = ''
-          ${attrs.postInstall or ""}
+        #postInstall = ''
+        #  ${attrs.postInstall or ""}
 
-          substituteInPlace $out/lib/udev/rules.d/83-hhd.rules \
+        #  substituteInPlace $out/lib/udev/rules.d/83-hhd.rules \
+        #    --replace-fail "/bin/chmod" "${lib.getExe' pkgs.coreutils "chmod"}"
+        #'';
+        postPatch = ''
+          ${attrs.postPatch or ""}
+
+          substituteInPlace usr/lib/udev/rules.d/83-hhd.rules \
             --replace-fail "/bin/chmod" "${lib.getExe' pkgs.coreutils "chmod"}"
         '';
       });
