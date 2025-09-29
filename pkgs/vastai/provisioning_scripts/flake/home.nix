6d96e38b59ac25e38c9a4d88b3f8738116d47ea8
Harry Askham <harryaskham@gmail.com>
2025-09-29 15:44:23 +0100

    [rapid commit; squash later]

---
+++ pkgs/vastai/provisioning_scripts/flake/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "root";
+  home.homeDirectory = "/root";
+  home.stateVersion = "25.05";
+  home.packages = [
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletions = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+  };
+}
--- pkgs/vastai/provisioning_scripts/nix.sh
+++ pkgs/vastai/provisioning_scripts/nix.sh
@@ -3,9 +3,6 @@
 cd /workspace/
 set -eo pipefail
 
-usermod -d /home/harry -m -g root -l harry user
-passwd -d harry
-
 apt update -y
 apt install curl -y
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
@@ -16,9 +13,17 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 touch $HOME/.no_auto_tmux
 export USER=$(id -un)
 export PATH="${PATH}:/nix/var/nix/profiles/default/bin"
-nix run home-manager/master -- init --switch
+
+mkdir -p $HOME/.config/home-manager
+for filename in home.nix flake.nix flake.lock; do
+  curl -o "$HOME/.config/home-manager/$filename" "https://raw.githubusercontent.com/harryaskham/collective-public/refs/heads/main/pkgs/vastai/provisioning_scripts/flake/$filename"
+done
+nix run home-manager/master -- switch
 
 for RC_FILE in $HOME/.bashrc $HOME/.zshrc; do
   echo 'export PATH="${PATH}:/nix/var/nix/profiles/default/bin"' >> $RC_FILE
   echo 'source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"' >> $RC_FILE
 done
+
+echo "/nix/var/nix/profiles/default/bin/zsh" >> /etc/shells
+chsh -s /nix/var/nix/profiles/default/bin/zsh
