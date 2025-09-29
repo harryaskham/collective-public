66c3c4b8a39f54dc9e4d0c3053bfebded9f610ee
Harry Askham <harryaskham@gmail.com>
2025-09-29 13:22:38 +0100

    [rapid commit; squash later]

--- pkgs/vastai-cli/provisioning_scripts/nix.sh
+++ pkgs/vastai-cli/provisioning_scripts/nix.sh
@@ -8,23 +8,23 @@ set -eo pipefail
 # Activate the main virtual environment
 . /venv/main/bin/activate
 
-apt update -y
-apt install curl -y
+sudo apt update -y
+sudo apt install curl -y
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
-  --extra-conf "sandbox = false" \
-  --init none \
-  --no-confirm
+# curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
+  # --extra-conf "sandbox = false" \
+  # --init none \
+  # --no-confirm
 
-export PATH="${PATH}:/nix/var/nix/profiles/default/bin"
-nix run home-manager/master -- init --switch
+# export PATH="${PATH}:/nix/var/nix/profiles/default/bin"
+# nix run home-manager/master -- init --switch
 
-EXTRA_SHELLRC=$(cat << EOF
-export PATH="\${PATH}:/nix/var/nix/profiles/default/bin"
-source "\$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-EOF
-)
+# EXTRA_SHELLRC=$(cat << EOF
+# export PATH="\${PATH}:/nix/var/nix/profiles/default/bin"
+# source "\$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+# EOF
+# )
 
-for RC_FILE in $HOME/.bashrc $HOME/.zshrc; do
-  echo "$EXTRA_SHELLRC" >> $RC_FILE
-done
+# for RC_FILE in $HOME/.bashrc $HOME/.zshrc; do
+  # echo "$EXTRA_SHELLRC" >> $RC_FILE
+# done
