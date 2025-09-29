1bc37dba1e119935b53b31a14894350626265295
Harry Askham <harryaskham@gmail.com>
2025-09-29 15:06:44 +0100

    [rapid commit; squash later]

--- pkgs/vastai-cli/provisioning_scripts/nix.sh
+++ pkgs/vastai-cli/provisioning_scripts/nix.sh
@@ -3,27 +3,22 @@
 cd /workspace/
 set -eo pipefail
 
+usermod -d /home/harry -m -g root -l harry user
+passwd -d harry
+
 apt update -y
 apt install curl -y
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
   --extra-conf "sandbox = false" \
   --init none \
   --no-confirm
-usermod -d /home/harry -m -g root -l harry user
 
-INIT_CMDS=$(cat << EOF
-touch \$HOME/.no_auto_tmux
-export USER=\$(id -un)
-export PATH="\${PATH}:/nix/var/nix/profiles/default/bin"
+touch $HOME/.no_auto_tmux
+export USER=$(id -un)
+export PATH="${PATH}:/nix/var/nix/profiles/default/bin"
 nix run home-manager/master -- init --switch
 
-for RC_FILE in \$HOME/.bashrc \$HOME/.zshrc; do
-  echo 'export PATH="\${PATH}:/nix/var/nix/profiles/default/bin"' >> \$RC_FILE
-  echo 'source "\$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"' >> \$RC_FILE
+for RC_FILE in $HOME/.bashrc $HOME/.zshrc; do
+  echo 'export PATH="${PATH}:/nix/var/nix/profiles/default/bin"' >> $RC_FILE
+  echo 'source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"' >> $RC_FILE
 done
-EOF
-)
-echo "$INIT_CMDS" > /workspace/init_script.sh
-chmod +x /workspace/init_script.sh
-bash -c /workspace/init_script.sh
-sudo -i -u harry bash -c /workspace/init_script.sh
