c7badb27733cf8f150b45debc3cf1b32185da82e
Harry Askham <harryaskham@gmail.com>
2025-09-29 14:00:05 +0100

    [rapid commit; squash later]

--- pkgs/vastai-cli/provisioning_scripts/nix.sh
+++ pkgs/vastai-cli/provisioning_scripts/nix.sh
@@ -2,22 +2,19 @@
 
 cd /workspace/
 set -eo pipefail
-. /venv/main/bin/activate
-
-touch $HOME/.no_auto_tmux
-
-usermod -d /home/harry -m -g root -l harry user
-su harry
-
 touch $HOME/.no_auto_tmux
 
 apt update -y
 apt install curl -y
-
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux \
   --extra-conf "sandbox = false" \
   --init none \
   --no-confirm
+
+usermod -d /home/harry -m -g root -l harry user
+su harry
+cd $HOME
+touch .no_auto_tmux
 
 export PATH="${PATH}:/nix/var/nix/profiles/default/bin"
 nix run home-manager/master -- init --switch
