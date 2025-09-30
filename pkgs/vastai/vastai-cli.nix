23bac3a939cf9a1fc7928f36bbd4e8c12ea1637a
Harry Askham <harryaskham@gmail.com>
2025-09-30 22:29:32 +0100

    [rapid commit; squash later]

--- pkgs/vastai/vastai-cli.nix
+++ pkgs/vastai/vastai-cli.nix
@@ -6,12 +6,13 @@ stdenv.mkDerivation rec {
   buildInputs = [
     (python3.withPackages (ps: [ ps.requests ]))
   ];
-  src = fetchurl {
-    url = "https://raw.githubusercontent.com/vast-ai/vast-cli/refs/tags/v${version}/vast.py";
-    hash = "sha256-mqgE8VwPJAbn+s+G8pI0mzouE0MX/2ikHBjNHVrP0ac=";
+  src = fetchFromGitHub {
+    owner = "vast-ai";
+    repo = "vast-cli";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-vtYSOfUmOvULLBULtabL15D82QxC2I00RbvCDrCoI3w=";
   };
-  dontUnpack = true;
   installPhase = ''
-    install -Dm755 $src $out/bin/vast
+    install -Dm755 $src/vast.py $out/bin/vast
   '';
 }
