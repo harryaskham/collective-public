f4a10cd257a6f4eaebc9c4122512bee5506d7ce7
7jane <7janeta@proton.me>
2025-12-13 13:07:34 +0000

    [rapid commit; squash later]

--- pkgs/collective-lib/binding.nix
+++ pkgs/collective-lib/binding.nix
@@ -51,34 +51,38 @@ rec {
   };
   swayMod = m: if (m == "super") then "Mod4" else m;
   swayMods = ms: map swayMod ms;
+  skhdMod = m: if (m == "super") then "cmd" else if (m == "$mod") then "alt" else m;
+  skhdMods = ms: map skhdMod ms;
   modPrefix = rec {
     # sway has "mod+mod+mod+key", empty mods are "key"
     sway = b: optionalString (b.mods != null && length b.mods > 0) "${strings.concatStringsSep "+" (swayMods b.mods)}+";
     i3 = sway;
     # Hyprland has "mod mod mod, keys", empty mods are ", keys"
     hypr = b: "${optionalString (b.mods != null && length b.mods > 0) (strings.concatStringsSep " " b.mods)}, ";
+    skhd = b: optionalString (b.mods != null && length b.mods > 0) "${strings.concatStringsSep " + " (skhdMods b.mods)} - ";
   };
   keySuffix = rec {
     sway = b: strings.concatStringsSep "+" b.keys;
     i3 = sway;
     hypr = b: strings.concatStringsSep " " b.keys;
+    skhd = b: strings.concatStringsSep " " b.keys;
   };
   mkBind = rec {
     sway = b: "${modPrefix.sway b}${keySuffix.sway b}";
     i3 = b: "${modPrefix.i3 b}${keySuffix.i3 b}";
     hypr = b: "${modPrefix.hypr b}${keySuffix.hypr b}";
+    skhd = b: "${modPrefix.skhd b}${keySuffix.skhd b}";
   };
   mkExec = rec {
     sway = cmd: "exec '${cmd}'";
     i3 = sway;
     hypr = cmd: "exec, ${cmd}";
+    skhd = cmd: cmd;
   };
   mkBindExec = rec {
     sway = (a: { ${mkBind.sway a.keybind} = mkExec.sway a.exec; });
     i3 = (a: { ${mkBind.i3 a.keybind} = mkExec.i3 a.exec; });
     hypr = (a: "${mkBind.hypr a.keybind}, ${mkExec.hypr a.exec}");
-  };
-  mkOptionalBindExec = {
-    hypr = (a: optionals a.enable [(mkBindExec.hypr a)]);
+    skhd = (a: "${mkBind.skhd a.keybind} : ${mkExec.skhd a.exec}");
   };
 }
