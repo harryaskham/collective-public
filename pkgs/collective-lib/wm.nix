6dad9088f7abb9f707090df9c4cb26d1046efbeb
7jane <7janeta@proton.me>
2025-12-13 15:53:14 +0000

    [rapid commit; squash later]

--- pkgs/collective-lib/wm.nix
+++ pkgs/collective-lib/wm.nix
@@ -278,7 +278,7 @@ in rec {
         moveWindowDown = runs "yabai -m window --swap south";
         moveWindowLeft = runs "yabai -m window --swap west";
         moveWindowRight = runs "yabai -m window --swap east";
-        openTerminal = runs ''open -n /Applications/kitty.app'';
+        openTerminal = runs ''open -n /Applications/Ghostty.app'';
         openLauncher = runs ''echo unimplemented'';
         reloadConfig = runs "yabai --restart-service; skhd --reload";
         floatCurrent = runs "yabai -m window --toggle float; yabai -m window --grid 4:4:1:1:2:2togglefloating";
