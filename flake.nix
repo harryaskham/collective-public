a3eed07235547a1d66b298b241798781c2439444
Harry Askham <harryaskham@gmail.com>
2026-04-18 16:40:11 +0100

    [rapid commit; squash later]

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     nix-reflect = {
       url = ./flakes/nix-reflect;
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.collective-public.nix-reflect.follows = "";
+      inputs.collective-public.inputs.nix-reflect.follows = "";
     };
 
     nix-parsec = {
